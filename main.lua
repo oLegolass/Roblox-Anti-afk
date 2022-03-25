688895e9ec412fb634adab35a347ea4155eeb03c
oLegolass <102174458+oLegolass@users.noreply.github.com>
2022-03-25 15:30:59 +0300

    Update and rename main.lua to AntiAfk.lua

--- main.lua
+++
@@ -1,4 +0,0 @@
-local v =96000000 --Maximal bit stream on client
--- (96000000 - 12) -- Maximal bit stream that server accept +-2
-local msg = ""..string.rep(" ",(v - 12))
-Libreria = require game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg,"All")
